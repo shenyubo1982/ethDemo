add71bf05adbaff9b791ae5189ce7b307d2319bf
shenyubo <shenyubo_1024@163.com>
2022-07-04 19:55:36 +0800

    docs:append readme and screenshots.

---
+++ build/keyManager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Describe：
+# Author：yubo.shen
+# Create Date：2022.06.01
+# Current version：v1.0
+# history version：v1.0
+# his version Desc：
+
+
+CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64 go build -o ../bin/ethKeyManage  ../main.go
+
